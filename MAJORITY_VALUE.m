25fde245f15e8084364c6d0f92e4e9a15928eb3c
Dracopower <psyjs17@exmail.nottingham.ac.uk>
2018-11-06 20:11:24 +0000

    Add files via upload

---
+++ MAJORITY_VALUE.m
@@ -0,0 +1,16 @@
+function [Majority] = MAJORITY_VALUE(targets)
+%UNTITLED3 Returns the majority value in target array
+MajNum = 0;
+MajLabel = [];
+for i = 1 : size(targets,1)
+    CurFreq = sum(targets(:,1)==targets(i,1)); %Checks how often label at poistion occurs in whole
+    
+    if (CurFreq > MajNum) %updates majority label
+        MajLabel = targets(i,1);
+        MajNum = CurFreq;
+    end
+end
+
+Majority = MajLabel; %returns the Majority Label
+end
+
---
+++ TreeStruct.m
@@ -0,0 +1,1 @@
+tree = struct('op', "", 'kids', {struct,struct}, 'class', [], 'attribute', [],'threshold', []);
