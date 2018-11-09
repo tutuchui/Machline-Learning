bdfbb82bbad31d9617127bb44955d2a035a9a854
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2018-11-09 09:54:43 +0000

    ConfusionMatrix

---
+++ ConfusionMatrix.m
@@ -0,0 +1,10 @@
+%confusion matrix function
+function [confusion_matrix, iter] = ConfusionMatrix (targets, outputs)
+    values = max(targets) - min(targets)+1;
+    confusion_matrix = zeros(values);
+    for i = 1:length(targets)
+        row = targets(i,1)+1;
+        col = outputs(i,1)+1;
+        confusion_matrix(row,col) =  confusion_matrix(row,col) + 1;
+    end
+end
