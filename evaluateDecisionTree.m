0151998ecc8e33bee020d26babc10649ae27a805
Yutong Wang <psyyw6@nottingham.ac.uk>
2018-11-13 20:07:14 +0000

    fix some spell error

--- evaluateDecisionTree.m
+++ evaluateDecisionTree.m
@@ -1,13 +1,13 @@
-function [cmat,recall,precision,fscore,misclassfiedNode] = evaluateDecisionTree(test_inputs,test_targets,tree)
+function [cmat,recall,precision,fscore,missclassifiedNode] = evaluateDecisionTree(test_inputs,test_targets,tree)
    outputs = zeros(15,1);
    j = 1;
-   misclassfiedNode = zeros(15,132);
+   missclassifiedNode = zeros(15,132);
    for n = 1:size(test_inputs,1)
         sample = test_inputs(n,:);
         class = validTree(tree,sample);
         outputs(n,1) = class;
         if class ~= test_targets(n,:)
-            misclassfiedNode(j,:) = sample;
+            missclassifiedNode(j,:) = sample;
             j = j + 1;
         end
    end
